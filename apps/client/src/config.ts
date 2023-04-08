670c0972cd0a5442a409a56dea813b714d4542f4
lannahirave <77197415+lannahirave@users.noreply.github.com>
2023-04-09 01:17:03 +0300

    Update config.ts
     - revert file to normal

--- apps/client/src/config.ts
+++ apps/client/src/config.ts
@@ -1,11 +1,11 @@
 export const config = {
   apiUrl: import.meta.env.DEV
-    ? 'https://api.minisi.krolebord.com'
+    ? 'http://localhost:8788'
     : import.meta.env.VITE_API_URL,
   wsUrl: import.meta.env.DEV
-    ? 'wss://api.minisi.krolebord.com'
+    ? 'ws://localhost:8788'
     : import.meta.env.VITE_WS_URL,
   packsUrl: import.meta.env.DEV
-    ? 'https://si-pack.krolebord.com'
+    ? 'https://dev.si-pack.krolebord.com'
     : import.meta.env.VITE_PACKS_URL,
 };
